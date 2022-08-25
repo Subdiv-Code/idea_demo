33474fd1276d1f8fca7c8435a23a34a897a69bd4
Nikita <109160998+Subdiv-Code@users.noreply.github.com>
2022-08-25 21:37:47 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ import { KTX2Loader } from '../js/KTX2Loader.js';
 	console.log(loader.setKTX2Loader);
 	//loader.setMeshoptDecoder( MeshoptDecoder );
 	
-	loader.load( 'model/all-castom/project_1/Project_1_export2.glb', function ( gltf ) {
+	loader.load( 'model/all-castom/project_1/Project_1_export1.glb', function ( gltf ) {
 		scene.add( gltf.scene );
 	});
 	loader.load( 'model/all/Env_Tree.glb', function ( gltf ) {
